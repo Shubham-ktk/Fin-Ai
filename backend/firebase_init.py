538d9bef7a5b60d6fd077d50ed8f66aeb29159d1
Shubham <khatkes855@gmail.com>
2026-01-04 20:11:47 +0530

    update backend and frontend

--- backend/firebase_init.py
+++ backend/firebase_init.py
@@ -11,21 +11,5 @@ firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 
-# import os
-# import firebase_admin
-# from firebase_admin import credentials, firestore
 
-# # Directory where this Python file lives
-# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-# # Service account JSON (downloaded from Firebase console → Settings → Service Accounts)
-# KEY_PATH = os.path.join(BASE_DIR, "firebase-key.json")
-
-# # Initialize app only if not already initialized
-# if not firebase_admin._apps:
-#     cred = credentials.Certificate(KEY_PATH)
-#     firebase_admin.initialize_app(cred)
-
-# # Firestore client
-# db = firestore.client()
 
